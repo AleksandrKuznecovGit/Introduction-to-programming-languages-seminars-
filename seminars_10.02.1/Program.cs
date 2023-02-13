1640763129ec1f230cf9354e55131d6f006c1f54
Александр Кузнецов <kuz888ka2@mail.ru>
2023-02-13 20:49:28 +0700

    add seminars_10.02.1

---
+++ seminars_10.02.1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+Console.WriteLine("Максимальное число - "+a + " минимальное число "+b);
+else Console.WriteLine("Максимальное число - " + b + " минимальное число " + a);
